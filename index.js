f990abdaf4f2b88e8fd829feb593ad40fa112ddf
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-21 19:21:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(name){
+}
 function appendKitten(){
   name.push()
   return name
 }
-}
-
 
 function destructivelyPrependKitten(name){
   
