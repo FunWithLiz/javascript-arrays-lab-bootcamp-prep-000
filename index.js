1a7e9aa06cbb9f15431625f8bba8e56f1175918a
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-21 20:02:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
-function appendKitten(add){
- name.push(add)
-  return name
-}
-
 
 function destructivelyPrependKitten(name){
   
